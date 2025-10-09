62ceb9dd13165ce00b85f2f5427faff498778a97
Devendra Milmile <d.h.milmile1@gmail.com>
2025-10-09 22:41:27 +0530

    Added Singleton Desing Pattern to UI Project

---
+++ UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Singleton;
+
+namespace UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====== Singleton Design Pattern Start ======\n");
+            var config = ConfigurationManager.Instance;
+            Console.WriteLine($"[Singleton] Loaded config: {config.ConnectionString}\n");
+            Console.WriteLine("====== Singleton Design Pattern End ======\n");
+        }
+    }
+}
